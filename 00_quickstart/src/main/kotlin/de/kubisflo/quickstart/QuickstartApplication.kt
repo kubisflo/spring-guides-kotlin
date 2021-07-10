b416741fd3ffa881fa8d536f0d148d9d460dae54
Florian Kubis <mail@kubisflo.de>
2021-07-10 23:16:45 +0200

    Add hello endpoint to QuickstartApplication.kt

--- 00_quickstart/src/main/kotlin/de/kubisflo/quickstart/QuickstartApplication.kt
+++ 00_quickstart/src/main/kotlin/de/kubisflo/quickstart/QuickstartApplication.kt
@@ -2,9 +2,19 @@ package de.kubisflo.quickstart
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 
+
+@RestController
 @SpringBootApplication
-class QuickstartApplication
+class QuickstartApplication {
+	@GetMapping("/hello")
+	fun hello(@RequestParam(value = "name", defaultValue = "World") name: String): String {
+		return "Hello $name!"
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<QuickstartApplication>(*args)
