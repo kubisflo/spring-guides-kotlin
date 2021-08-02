8aa28240dd7d478bfe0f087a1a44d38830ead3f9
Florian Kubis <florian.kubis@refugium22.de>
2021-08-02 13:10:01 +0200

    Start using lateinit vars for @Autowired properties
    See https://kotlinlang.org/docs/properties.html#late-initialized-properties-and-variables

--- 06_testing-web/src/test/kotlin/de/kubisflo/testingweb/SmokeTest.kt
+++ 06_testing-web/src/test/kotlin/de/kubisflo/testingweb/SmokeTest.kt
@@ -10,7 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest
 class SmokeTest {
 
     @Autowired
-    private val controller: HomeController? = null
+    private lateinit var controller: HomeController
 
     @Test
     @Throws(Exception::class)
