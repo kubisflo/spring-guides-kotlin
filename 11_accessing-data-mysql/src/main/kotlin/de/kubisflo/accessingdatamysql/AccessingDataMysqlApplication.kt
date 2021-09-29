a38c417f6b008bfa3673c9b273a47da814add239
Florian Kubis <florian.kubis@refugium22.de>
2021-09-29 15:54:22 +0200

    Add project for accessing-data-mysql guide

--- 11_accessing-data-mysql/src/main/kotlin/de/kubisflo/accessingdatamysql/AccessingDataMysqlApplication.kt
+++ 11_accessing-data-mysql/src/main/kotlin/de/kubisflo/accessingdatamysql/AccessingDataMysqlApplication.kt
@@ -2,6 +2,8 @@ package de.kubisflo.accessingdatamysql
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.annotation.Id
+import org.springframework.data.repository.CrudRepository
 
 @SpringBootApplication
 class AccessingDataMysqlApplication
@@ -9,3 +11,12 @@ class AccessingDataMysqlApplication
 fun main(args: Array<String>) {
 	runApplication<AccessingDataMysqlApplication>(*args)
 }
+
+data class User(
+	@Id
+	val id: Int,
+	val name: String,
+	val email: String
+)
+
+interface UserRepository : CrudRepository<User, Int>
